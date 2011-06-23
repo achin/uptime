edc198c1696fa2662f3a0c9913ba358510dedc19
Alex Chin <alex.ba.chin@gmail.com>
2011-06-23 01:06:01 -0400

    Added compojure.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject uptime "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [congomongo "0.1.4-SNAPSHOT"]])
+                 [congomongo "0.1.4-SNAPSHOT"]
+		 [compojure "0.6.3"]]
+  :dev-dependencies [[lein-ring "0.4.3"]]
+  :ring {:handler uptime.core/app})
--- src/uptime/core.clj
+++ src/uptime/core.clj
@@ -1,5 +1,9 @@
 (ns uptime.core
-  (:use somnium.congomongo))
+  (:use somnium.congomongo
+        compojure.core
+        clojure.contrib.json)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]))
 
 (mongo! :db "uptime")
 
@@ -27,3 +31,17 @@
 (defn remove-service [id]
   (when-let [s (get-service id)]
     (destroy! :services s)))
+
+(defroutes main-routes
+  (GET "/" [] "<h1>Uptime!</h1>")
+  (GET "/service" []
+       (json-str (list-services)))
+  (GET "/service/:id" [id]
+       (if-let [s (get-service id)]
+         (json-str s)
+         {:status 404}))
+  (route/resources "/")
+  (route/not-found "Page not found."))
+
+(def app
+  (handler/site main-routes))
