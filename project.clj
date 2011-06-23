6894abef39028a805eed05c4708c9ab122f1da66
Alex Chin <alex.ba.chin@gmail.com>
2011-06-23 00:05:23 -0400

    Added congomongo dep.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject uptime "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [congomongo "0.1.4-SNAPSHOT"]])
