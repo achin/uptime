22a20b7a78ee37842ab21bb0e2e9f71d193b1455
Alex Chin <alex.ba.chin@gmail.com>
2011-06-23 00:16:37 -0400

    Basic mongodb.

--- src/uptime/core.clj
+++ src/uptime/core.clj
@@ -1,9 +1,22 @@
-(ns uptime.core)
+(ns uptime.core
+  (:use somnium.congomongo))
+
+(mongo! :db "uptime")
+
+(defn list-services []
+  (fetch :services))
+
+(defn- uuid []
+  (.toString (java.util.UUID/randomUUID)))
 
 (defn add-service [name url]
-  {:name name
-   :url url
-   :votes []}) 
+  (insert! :services {:_id (uuid)
+                     :name name
+                     :url url
+                     :votes []})) 
+
+(defn get-service [id]
+  (fetch-one :services :where {:_id id}))
 
 (defn vote-service [s state date comment]
   (let [v {:state state :date date :comment comment}]
