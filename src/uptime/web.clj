35b79c092911b40aba719f7e36f8b1353147aa33
Alex Chin <achin@texeltek.com>
2011-06-28 20:39:13 -0400

    Added DELETE.

--- src/uptime/web.clj
+++ src/uptime/web.clj
@@ -5,24 +5,24 @@
             [compojure.handler :as handler]
             [uptime.db :as db]))
 
-(defn- json-response [s & {status :status headers :headers}]
+(defn json-response [s & {status :status headers :headers}]
   (let [json (json-str s)]
     {:status (or status 200)
      :headers (merge {"Content-Type" "application/json"} headers)
      :body json}))
 
-(defn- root []
+(defn root []
   "<h1>Uptime!</h1>")
 
-(defn- list-services []
+(defn list-services []
   (json-response (db/list-services)))
 
-(defn- get-service [id]
+(defn get-service [id]
   (if-let [s (db/get-service id)]
     (json-response s)
     {:status 404}))
 
-(defn- add-service [name url]
+(defn add-service [name url]
   (if-let [s (db/add-service name url)]
     (let [url (str "/service/" (:_id s))]
       (json-response s
@@ -30,15 +30,27 @@
                      :headers {"Location" url}))
     {:status 500}))
 
+(defn remove-service [id]
+  (if (db/remove-service id)
+    {:status 204}
+    {:status 500}))
+
 (defroutes main-routes
   (GET "/" []
        (root))
+  
   (GET "/service" []
        (list-services))
+  
   (GET "/service/:id" [id]
        (get-service id))
+  
   (POST "/service" [name url]
         (add-service name url))
+  
+  (DELETE "/service/:id" [id]
+          (remove-service id))
+  
   (route/resources "/")
   (route/not-found "Page not found."))
 
