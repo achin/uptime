4cf2bae4409dccda23e37174e57539b490b31d1a
Alex Chin <achin@texeltek.com>
2011-07-05 08:36:19 -0400

    Added midje. You might want to also install lein-midje.
    lein plugin install lein-midje 1.0.2

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [congomongo "0.1.4-SNAPSHOT"]
 		 [compojure "0.6.3"]]
-  :dev-dependencies [[lein-ring "0.4.3"]]
+  :dev-dependencies [[lein-ring "0.4.3"]
+                     [midje "1.1.1"]]
   :ring {:handler uptime.web/app})
