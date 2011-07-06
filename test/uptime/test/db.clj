4bc8183076a9d7632a8824db4530ab59a017a78d
Alex Chin <achin@texeltek.com>
2011-07-05 20:21:58 -0400

    Figured out how to declare a var in another namespace.

--- test/uptime/test/db.clj
+++ test/uptime/test/db.clj
@@ -3,4 +3,17 @@
   (:use clojure.test)
   (:use midje.sweet))
 
-(fact (+ 1 1) => 3)
+(defmacro remote-declare [n]
+  (let [ns (namespace n) 
+        v (name n) 
+        orig-ns (str *ns*)] 
+    `(do (in-ns '~(symbol ns)) 
+         (declare ~(symbol v)) 
+         (in-ns '~(symbol orig-ns)))))
+
+(remote-declare somnium.congomogo/fetch)
+
+(fact
+ (list-services) => :dummy-list
+ (provided
+  (somnium.congomongo/fetch :services) => :dummy-list))
